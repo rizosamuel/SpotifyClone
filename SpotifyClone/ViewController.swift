d0a34085778b85f5c180ec68f2c2737debd72b8d
Rijo <rizosamuel1995@gmail.com>
2022-01-01 18:16:27 +0530

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Rijo Samuel on 01/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
